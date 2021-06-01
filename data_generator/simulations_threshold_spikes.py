3f3642d5ab08f955c81e21b680f16f75d105dad4
kopalgarg <kopalgarg24@gmail.com>
2021-06-01 12:53:18 -0600

    Update simulations_threshold_spikes.py

--- data_generator/simulations_threshold_spikes.py
+++ data_generator/simulations_threshold_spikes.py
@@ -39,7 +39,7 @@ plt.xlabel('Frequency [Hz]')
 plt.grid()
 
 
-def main(n_samples, plot, Tt=24):
+def main(plot,n_samples,  Tt=24):
     signal_in = []
     thresholds = []
     trend_style=[]
@@ -82,104 +82,173 @@ def main(n_samples, plot, Tt=24):
         x_test_n = np.stack([x1_test_lpf, x2_test_lpf, x3_test_lpf],axis=1)
 
     kappa=1.5
-    y_train = []
-    ground_truth_importance_train=[]
-    ground_truth_importance_test=[]
+    y_train_f0 = []
+    y_train_f1 = []
+    y_train_f2 = []
+
+    y_test_f0 = []
+    y_test_f1 = []
+    y_test_f2 = []
+
+    ground_truth_importance_train_f0=[]
+    ground_truth_importance_train_f1=[]
+    ground_truth_importance_train_f2=[]
+
+    ground_truth_importance_test_f0=[]
+    ground_truth_importance_test_f1=[]
+    ground_truth_importance_test_f2=[]
+
+
     pvec=[0.7,0.3]
     lamda_poisson = 3
     
     for i in range(n_train):
-        gt_t = np.zeros((Tt))
+
+        # f0
+        gt_t_f0 = np.zeros((Tt))
         add_spikes = np.random.choice([0,1],1,p=pvec)
         if add_spikes:
             nts = np.random.poisson(lamda_poisson,1)
+            
         else:
             nts=0
-        t_vec = np.sort(np.random.choice(list(range(Tt)),nts,replace=False))
-        x_train_n[i,0,t_vec] +=   kappa
-        if len(t_vec)>0:
-            gt_t[t_vec[0]] = 1
-            
-        y_vec = np.zeros((Tt))
-        if len(t_vec)>0:
-            y_vec[t_vec[0]:] = 1
-        y_train.append(y_vec)
-        ground_truth_importance_train.append(gt_t)
+        t_vec_f0 = np.sort(np.random.choice(list(range(Tt)),nts,replace=False))
         
-        add_spikes = np.random.choice([0,1],1,p=pvec)
-        if add_spikes:
-            nts = np.random.poisson(lamda_poisson,1)
-        else:
-            nts=0
-        #nts=1
-        t = np.random.choice(list(range(Tt)),nts,replace=False)
-        if len(t)>0:
-            x_train_n[i,1,t] +=  kappa
+        x_train_n[i,0,t_vec_f0] +=   kappa
+        
+        if len(t_vec_f0)>0:
+            gt_t_f0[t_vec_f0] = 1
             
+        y_vec_f0 = np.zeros((Tt))
+        if len(t_vec_f0)>0:
+            y_vec_f0[t_vec_f0] = 1
+        y_train_f0.append(y_vec_f0)
+        ground_truth_importance_train_f0.append(gt_t_f0)
+
+        # f1
+        gt_t_f1 = np.zeros((Tt))
         add_spikes = np.random.choice([0,1],1,p=pvec)
         if add_spikes:
             nts = np.random.poisson(lamda_poisson,1)
         else:
             nts=0
-        #nts=1
-        t = np.random.choice(list(range(Tt)),nts,replace=False)
-        if len(t)>0:
-            x_train_n[i,2,t] +=  kappa
+        t_vec_f1 = np.sort(np.random.choice(list(range(Tt)),nts,replace=False))
+        x_train_n[i,1,t_vec_f1] +=   kappa
+        if len(t_vec_f1)>0:
+            gt_t_f1[t_vec_f1] = 1
+            
+        y_vec_f1 = np.zeros((Tt))
+        if len(t_vec_f1)>0:
+            y_vec_f1[t_vec_f1] = 1
 
-    y_test = []
+        y_train_f1.append(y_vec_f1)
+        ground_truth_importance_train_f1.append(gt_t_f1)
+
+        # f2
+        gt_t_f2 = np.zeros((Tt))
+        add_spikes = np.random.choice([0,1],1,p=pvec)
+        if add_spikes:
+            nts = np.random.poisson(lamda_poisson,1)
+        else:
+            nts=0
+        t_vec_f2 = np.sort(np.random.choice(list(range(Tt)),nts,replace=False))
+        x_train_n[i,2,t_vec_f2] +=   kappa
+        if len(t_vec_f2)>0:
+            gt_t_f2[t_vec_f2] = 1
+            
+        y_vec_f2 = np.zeros((Tt))
+        if len(t_vec_f2)>0:
+            y_vec_f2[t_vec_f2] = 1
+        y_train_f2.append(y_vec_f2)
+        ground_truth_importance_train_f2.append(gt_t_f2)
+
+
     for i in range(x_test_n.shape[0]):
-        gt_t = np.zeros((Tt))
+        # f0
+        gt_t_f0 = np.zeros((Tt))
         add_spikes = np.random.choice([0,1],1,p=pvec)
         if add_spikes:
             nts = np.random.poisson(lamda_poisson,1)
-        else:
-            nts=0
-
-        t_vec = np.sort(np.random.choice(list(range(Tt)),nts,replace=False))
-        if len(t_vec)>0:
-            x_test_n[i,0,t_vec] +=   kappa
-
-        if len(t_vec)>0:
-            gt_t[t_vec[0]] = 1
             
-        y_vec = np.zeros((Tt))
-        if len(t_vec)>0:
-            y_vec[t_vec[0]:] = 1
-        y_test.append(y_vec)
-        ground_truth_importance_test.append(gt_t)
+        else:
+            nts=0
+        t_vec_f0 = np.sort(np.random.choice(list(range(Tt)),nts,replace=False))
+        
+        x_test_n[i,0,t_vec_f0] +=   kappa
+        
+        if len(t_vec_f0)>0:
+            gt_t_f0[t_vec_f0] = 1
+            
+        y_vec_f0 = np.zeros((Tt))
+        if len(t_vec_f0)>0:
+            y_vec_f0[t_vec_f0] = 1
+        y_test_f0.append(y_vec_f0)
+        ground_truth_importance_test_f0.append(gt_t_f0)
 
+        # f1
+        gt_t_f1 = np.zeros((Tt))
         add_spikes = np.random.choice([0,1],1,p=pvec)
         if add_spikes:
             nts = np.random.poisson(lamda_poisson,1)
         else:
             nts=0
-        #nts=1
-        t = np.random.choice(list(range(Tt)),nts,replace=False)
-        if len(t)>0:
-            x_test_n[i,1,t] +=  kappa
+        t_vec_f1 = np.sort(np.random.choice(list(range(Tt)),nts,replace=False))
+        x_test_n[i,1,t_vec_f1] +=   kappa
+        if len(t_vec_f1)>0:
+            gt_t_f1[t_vec_f1] = 1
+            
+        y_vec_f1 = np.zeros((Tt))
+        if len(t_vec_f1)>0:
+            y_vec_f1[t_vec_f1] = 1
 
+        y_test_f1.append(y_vec_f1)
+        ground_truth_importance_test_f1.append(gt_t_f1)
+
+        # f2
+        gt_t_f2 = np.zeros((Tt))
         add_spikes = np.random.choice([0,1],1,p=pvec)
         if add_spikes:
             nts = np.random.poisson(lamda_poisson,1)
         else:
             nts=0
-        #nts=1
-        t = np.random.choice(list(range(Tt)),nts,replace=False)
-        if len(t)>0:
-            x_test_n[i,2,t] +=  kappa
+        t_vec_f2 = np.sort(np.random.choice(list(range(Tt)),nts,replace=False))
+        x_test_n[i,2,t_vec_f2] +=   kappa
+        if len(t_vec_f2)>0:
+            gt_t_f2[t_vec_f2] = 1
+            
+        y_vec_f2 = np.zeros((Tt))
+        if len(t_vec_f2)>0:
+            y_vec_f2[t_vec_f2] = 1
+        y_test_f2.append(y_vec_f2)
+        ground_truth_importance_test_f2.append(gt_t_f2)
 
-    y_train = np.array(y_train)
-    y_test = np.array(y_test)
 
 
-    ground_truth_importance_train = np.array(ground_truth_importance_train)
-    ground_truth_importance_test = np.array(ground_truth_importance_test)
-    
+    y_train_f0 = np.array(y_train_f0)
+    y_train_f1 = np.array(y_train_f1)
+    y_train_f2 = np.array(y_train_f2)
+
+    y_test_f0 = np.array(y_test_f0)
+    y_test_f1 = np.array(y_test_f1)
+    y_test_f2 = np.array(y_test_f2)
+
     ground_truth_imp_train_mat = np.zeros((n_train,3,Tt))
     ground_truth_imp_test_mat = np.zeros((n_test,3,Tt))
-    
-    ground_truth_imp_train_mat[:,0,:] = ground_truth_importance_train
-    ground_truth_imp_test_mat[:,0,:] = ground_truth_importance_test
+
+    ground_truth_imp_train_mat[:,0,:] = ground_truth_importance_train_f0
+    ground_truth_imp_train_mat[:,1,:] = ground_truth_importance_train_f1
+    ground_truth_imp_train_mat[:,2,:] = ground_truth_importance_train_f2
+
+    ground_truth_imp_test_mat[:,0,:] = ground_truth_importance_test_f0
+    ground_truth_imp_test_mat[:,1,:] = ground_truth_importance_test_f1
+    ground_truth_imp_test_mat[:,2,:] = ground_truth_importance_test_f2
+
+    y_train = y_train_f0 + y_train_f1 + y_train_f2
+    y_train = np.where(y_train>1, 1, y_train) 
+
+    y_test = y_test_f0 + y_test_f1 + y_test_f2
+    y_test = np.where(y_test>1, 1, y_test) 
+
  
     if plot:
         for i in range(x_train_n.shape[0]):
@@ -198,7 +267,6 @@ def main(n_samples, plot, Tt=24):
 
             plt.legend()
             plt.show()
-
     return x_train_n[:,:,:],y_train,x_test_n[:,:,:],y_test,thresholds_train,thresholds_test, ground_truth_imp_train_mat[:,:,:], ground_truth_imp_test_mat[:,:,:]
 
 def generate_sample(plot, Tt=24):
